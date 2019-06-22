a86b0d81fc0eb26b02931bdafbb33491b1f8948d
rlaalsrb2305 <51859951+rlaalsrb2305@users.noreply.github.com>
2019-06-23 02:06:37 +0900

    Update JAEIN.py

--- JAEIN.py
+++ JAEIN.py
@@ -1,4 +1,5 @@
 import discord
+import random
 
 client = discord.Client()
 
@@ -15,7 +16,7 @@ async def on_ready():
     print("!명령어,!변태순위 입니다")
     print("===========")
 
-    await client.change_presence(game=discord.Game(name="북한이 먼저다 | !재이니 한마디", type=1))
+    await client.change_presence(game=discord.Game(name="북한이 먼저다 | !재이니한마디", type=1))
 
 
 @client.event
@@ -28,6 +29,16 @@ async def on_message(message):
 
     if message.content.startswith("!재이니한마디"):
         channel = message.channel
+        talk = "북한이 쌀을 세금을 너네엄마를 너네아빠를 우리나라을 대한민국을 북한을 섹파를 노휘빈을"
+        talkchoice = talk.split(" ")
+        talknumber = random.randint(1, len(talkchoice))
+        talkresult = talkchoice[talknumber-1]
+
+        last = "내놔라 가져가라 뺏는다"
+        lastchoice = last.split(" ")
+        lastnumber = random.randint(1, len(lastchoice))
+        lastresult = lastchoice[lastnumber-1]
+
         embed = discord.Embed(
             title='크흠큼큼',
             description='어..궁민 여러분..',
@@ -35,7 +46,7 @@ async def on_message(message):
         )
 
         embed.set_footer(text='이상입다')
-        embed.add_field(name='.', value='샤뢈이 먼줘다!', inline=False)
+        embed.add_field(name='.', value=talkresult + lastresult, inline=False)
         embed.set_image(url="https://search.pstatic.net/common?type=a&size=120x150&quality=95&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2Fportrait%2F201611%2F20161122215734443.png")
 
         await client.send_message(channel, embed=embed)
